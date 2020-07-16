d5ed78cb095728a779811e0c7a19298dd29df074
zdazx <zhangduo0227@163.com>
2020-07-16 22:05:20 +0800

    infinite value in differnet condition

--- src/test/java/com/cultivation/javaBasic/ObjectTest.java
+++ src/test/java/com/cultivation/javaBasic/ObjectTest.java
@@ -67,7 +67,7 @@ class ObjectTest {
 
         // TODO: please modify the following code to pass the test
         // <--start
-        final int expected = 0;
+        final int expected = 5;
         // --end-->
 
         assertEquals(expected, value);
@@ -89,7 +89,7 @@ class ObjectTest {
         // TODO: please modify the following code to pass the test.
         // You can only choose from `sameReference` and `instanceCreatedByMethod`
         // <--start
-        final Object expected = null;
+        final Object expected = sameReference;
         // --end-->
 
         assertEquals(expected, objectReference);
@@ -103,7 +103,7 @@ class ObjectTest {
 
         // TODO: please modify the following code to pass the test
         // <--start
-        final String expected = null;
+        final String expected = "Updated Name";
         // --end-->
 
         assertEquals(expected, instance.getName());
