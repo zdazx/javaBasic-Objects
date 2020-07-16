5f57f82050182947b10fd7f3acbf8c001c686b37
zdazx <zhangduo0227@163.com>
2020-07-16 21:19:10 +0800

    extends exception

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/StringFormatException.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/StringFormatException.java
@@ -7,14 +7,14 @@ public class StringFormatException extends Exception {
     public StringFormatException(String message) {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        super(message);
         // --end-->
     }
 
     public StringFormatException(String message, Throwable cause) {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        super(message, cause);
         // --end-->
     }
 }
