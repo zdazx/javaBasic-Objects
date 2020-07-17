7e4bafef1278ca42045d6dc242a02421475c64e7
zdazx <zhangduo0227@163.com>
2020-07-17 17:55:57 +0800

    update should use Throwable call stack trace info

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
@@ -4,8 +4,10 @@ public class StackFrameHelper {
     public static String getCurrentMethodName() {
         // TODO: please modify the following code to pass the test
         // <--start
-        StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[2];
-        return stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName();
+//        StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[2];
+//        return stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName();
         // --end-->
+        StackTraceElement stackTraceElement = new Throwable().getStackTrace()[1];
+        return stackTraceElement.getClassName() + "." + stackTraceElement.getMethodName();
     }
 }
